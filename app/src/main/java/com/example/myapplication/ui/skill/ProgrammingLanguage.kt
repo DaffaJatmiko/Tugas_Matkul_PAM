1127eef250c7b7306e0298f4d9faa9482d4e0a20
DaffaJatmiko <daffamiko123@gmail.com>
2023-10-06 22:14:42 +0700

    Update Tugas 4 (Clean Code)

--- app/src/main/java/com/example/myapplication/ui/skill/ProgrammingLanguage.kt
+++ app/src/main/java/com/example/myapplication/ui/skill/ProgrammingLanguage.kt
@@ -1,3 +1,29 @@
 package com.example.myapplication.ui.skill
 
-data class ProgrammingLanguage(val image: Int, val name: String)
+import android.os.Parcel
+import android.os.Parcelable
+
+data class ProgrammingLanguage(val image: Int, val name: String) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readInt(),
+        parcel.readString()!!
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeInt(image)
+        parcel.writeString(name)
+    }
+    override fun describeContents(): Int {
+        return 0
+    }
+    companion object CREATOR : Parcelable.Creator<ProgrammingLanguage> {
+        override fun createFromParcel(parcel: Parcel): ProgrammingLanguage {
+            return ProgrammingLanguage(parcel)
+        }
+
+        override fun newArray(size: Int): Array<ProgrammingLanguage?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
+
